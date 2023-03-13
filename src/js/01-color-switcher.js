c6b13975a2fd36b5d901a6ea28f711fa4ad05166
Voinalovych91 <Slemrulll@gmail.com>
2023-03-13 20:55:49 +0200

    HW01-03

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+const bodyEl = document.body;
+let timerId = null;
 
+btnStart.addEventListener('click', onBtnStartClick);
+btnStop.addEventListener('click', onBtnStopClick);
+
+btnStop.disabled = true;
+
+function onBtnStartClick(event) {
+    timerId = setInterval(changeColor, 1000);
+
+    event.target.disabled = true;
+    btnStop.disabled = false;
+}
+
+function onBtnStopClick(event) {
+    clearInterval(timerId);
+
+    event.target.disabled = true;
+    btnStart.disabled = false;
+
+}
+
+function changeColor() {
+    const colorRandom = getRandomHexColor();
+
+    bodyEl.style.backgroundColor = colorRandom;
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,42 @@
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve({ position, delay });
+      } else {
+        // Reject
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+
+  return promise;
+}
+
+const formRef = document.querySelector('.form');
+formRef.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  let delay = Number(event.currentTarget.delay.value);
+  const step = Number(event.currentTarget.step.value);
+  const amount = Number(event.currentTarget.amount.value);
+
+  for (let i = 1; i <= amount; i++) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        console.log(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        console.log(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+    delay += step;
   }
 }
